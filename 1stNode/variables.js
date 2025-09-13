34e39010b7e88212fbfc4f826d7f69c351089ce1
Poushie17 <165271859+Poushie17@users.noreply.github.com>
2025-09-13 17:43:12 +0000

    Container and node

---
+++ 1stNode/variables.js
@@ -0,0 +1,18 @@
+const accountId = 20250913;   
+let accountEmail = "poushie@gmail.com";
+let accountPassword = "skillConnect2025";
+let accountCity = "Konabari";
+let accountState = "Dhaka";
+
+accountEmail = "poushie123@gmail.com";
+accountPassword = "securePass123";
+accountCity = "Gazipur";
+
+
+console.table({
+  ID: accountId,
+  Email: accountEmail,
+  Password: accountPassword,
+  City: accountCity,
+  State: accountState
+});
