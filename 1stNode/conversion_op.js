3e8057c592063ded2f1a89f6f7302a18be92e0e9
Poushie17 <165271859+Poushie17@users.noreply.github.com>
2025-09-14 00:13:40 +0600

    conversion operation

---
+++ 1stNode/conversion_op.js
@@ -0,0 +1,40 @@
+"use strict";
+
+
+let score = "poushie";
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score);
+
+// "33"   => 33
+// "33abc" => NaN
+// true   => 1
+// false  => 0
+
+let isLoggedIn = "poushie";
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+
+
+let someNumber = 33;
+let stringNumber = String(someNumber);
+
+let value = 3;
+let negValue = -value;
+
+
+let str1 = "hello";
+let str2 = " poushie";
+
+let str3 = str1 + str2;
+
+
+
+let num1, num2, num3;
+num1 = num2 = num3 = 2 + 2; // all 3 = 4
+
+
+++gameCounter;
+console.log(gameCounter); // 101
