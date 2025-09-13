fd6f9fe953711cba82d45093715a473b0a68f463
Poushie17 <165271859+Poushie17@users.noreply.github.com>
2025-09-14 00:08:50 +0600

    datatype

---
+++ 1stNode/datatypes.js
@@ -0,0 +1,20 @@
+"use strict"; 
+
+
+
+console.log(3 + 3); 
+
+console.log("Poushie");
+
+// variables
+let name = "Poushie";
+let age = 21;
+let isLoggedIn = true;
+let state; // undefined
+
+
+
+
+
+console.log(typeof undefined);
+console.log(typeof null); 
