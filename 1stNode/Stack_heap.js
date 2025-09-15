2f0e11067ee7857961280dd249058cd83bd5b39c
Poushie17 <165271859+Poushie17@users.noreply.github.com>
2025-09-15 22:28:32 +0600

    Create Stack_heap.js

---
+++ 1stNode/Stack_heap.js
@@ -0,0 +1,17 @@
+
+
+// stack premitive ,heap non premitive
+let name="poushie"
+let anothername=name
+ anothername="mim"
+ console.log(name)
+console.log(anothername)
+
+let user1={
+    email:"use1r@gmail.com",
+    upi : "12345"
+}
+
+let user2=user1
+user2.email="user2.gmail.com"
+console.log([user1.email,user2.email])
