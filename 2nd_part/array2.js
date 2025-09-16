f004480449648d86dea332b8526723e41e92c7da
Poushie17 <165271859+Poushie17@users.noreply.github.com>
2025-09-16 23:54:09 +0600

    Create array2.js

---
+++ 2nd_part/array2.js
@@ -0,0 +1,16 @@
+const arr1=[1,2,3,4,8]
+const arr2=["a","b","c"]
+// /arr1.push(arr2)
+// console.log(arr1)
+arr3=[3,8]
+// const arrspd=[...arr1,...arr2,...arr3]
+// console.log(arrspd)
+ arrf=[1,2,4,[3,9,[6,8]]]
+ const dpt=arrf.flat(Infinity)
+ console.log(dpt)
+ console.log(Array.isArray("poushie"))
+ console.log(Array.from("poushie"))
+ console.log(Array.of("poushie","mim","xx"))
+
+
+ 
