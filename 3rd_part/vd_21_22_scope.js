f24b6d136ba0c171f1f76bed0bea9b3f3dc902d8
Poushie17 <165271859+Poushie17@users.noreply.github.com>
2025-09-19 20:06:57 +0600

    Create vd_21_22_scope.js

---
+++ 3rd_part/vd_21_22_scope.js
@@ -0,0 +1,35 @@
+// // let a=20
+// function f1(){
+//     const a=40
+//     function f2(){
+//         const b=9
+//         console.log(a)
+//     }
+//    // console.log(b)
+//     //f2()
+    
+// }
+// f1()
+if (true)
+{
+    const a=3
+    if(a===3){
+        const b=20
+        //console.log(a+b)
+
+    }
+   // console.log(b)
+}
+//console.log(a)
+
+console.log(add1(7))
+function add1(n){
+    return n+1
+
+} 
+add1(7)
+
+const add2=function (num){
+    return num+2
+}
+add2(7)
