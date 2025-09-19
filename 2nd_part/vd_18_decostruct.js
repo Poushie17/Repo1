100cdc118e6cc42da49f11a2ebb43ad3a0a531b0
Poushie17 <165271859+Poushie17@users.noreply.github.com>
2025-09-19 16:53:27 +0600

    Create vd_18_decostruct.js

---
+++ 2nd_part/vd_18_decostruct.js
@@ -0,0 +1,16 @@
+const user={}
+user.id="1ab12"
+user.name="poush"
+user.login=true
+
+const {name}=user
+// console.log(name)
+
+const {name:n}=user
+//console.log(n)
+
+// {
+//     "name": "poushie",
+//     "id": 1234,
+//     "age": 20
+// }
