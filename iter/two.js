98385c8a6d4fd14a11d3907fdbab0b2986898f94
Poushie17 <165271859+Poushie17@users.noreply.github.com>
2025-09-22 01:35:06 +0600

    Create two.js

---
+++ iter/two.js
@@ -0,0 +1,20 @@
+
+let attempt = 0;
+while (attempt <= 10) {
+    // console.log(`Attempt number: ${attempt}`);
+    attempt += 2; // increment by 2 each time
+}
+
+let courses = ["Math", "CSE", "English"];
+let i = 0;
+while (i < courses.length) {
+    // console.log(`Enrolled in: ${courses[i]}`);
+    i++;
+}
+
+// Do-while loop: grades check
+let grade = 11;
+do {
+    console.log(`Current grade: ${grade}`);
+    grade++;
+} while (grade <= 10); // this will run at least once
