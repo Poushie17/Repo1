7488ea44b8a0926c422a6441c908fc6a37f18dab
Poushie17 <165271859+Poushie17@users.noreply.github.com>
2025-09-22 01:23:17 +0600

    Create switch.js

---
+++ 4th_part/switch.js
@@ -0,0 +1,19 @@
+const favColor = "blue"
+
+switch (favColor) {
+    case "red":
+        console.log("Red is bold.");
+        break;
+    case "green":
+        console.log("Green feels calm.");
+        break;
+    case "blue":
+        console.log("Blue is classic.");
+        break;
+    case "black":
+        console.log("Black goes with everything.");
+        break;
+    default:
+        console.log("That color’s unique!");
+        break;
+}
