13cc15bb825f2433c6fd45cde3e3f192f1031cfa
Poushie17 <165271859+Poushie17@users.noreply.github.com>
2025-10-05 02:05:21 +0600

    Create getter_setter.js

---
+++ 1stNode/advance/getter_setter.js
@@ -0,0 +1,25 @@
+
+class User {
+    constructor(email, password){
+        this.email = email;
+        this.password = password
+    }
+
+    get email(){
+        return this._email.toUpperCase()
+    }
+    set email(value){
+        this._email = value
+    }
+
+    get password(){
+        return `${this._password}poushie`
+    }
+
+    set password(value){
+        this._password = value
+    }
+}
+
+const hitesh = new User("xx@gmail.com", "abc")
+console.log(hitesh.email);
